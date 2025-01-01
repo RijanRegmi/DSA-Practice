ec17656910e49279615fc26f65c0ec4df06c6857
rijan <rijanregmi111@gmail.com>
2025-01-01 08:16:26 +0545

    code updated

--- Maximum Subarray/Number6.java
+++ Maximum Subarray/Number6.java
@@ -1,24 +1,31 @@
 public class Number6 {
     public static int maxSubArray(int[] nums) {
-        int max = nums[1];
+        int max = Integer.MIN_VALUE;
         int sum = 0;
         int start = 0;
         int end = 0;
-        int temstart = 0;
+        int tempStart = 0;
 
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
+
             if (sum > max) {
                 max = sum;
-                start = temstart;
+                start = tempStart;
                 end = i;
             }
+
             if (sum < 0) {
-                temstart = i + 1;
                 sum = 0;
+                tempStart = i + 1;
             }
         }
 
+        // Print the indices of the maximum subarray
+        System.out.println("Start Index: " + start);
+        System.out.println("End Index: " + end);
+
+        return max;
     }
 
     public static void main(String[] args) {
